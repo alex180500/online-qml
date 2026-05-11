2057e25d030e563277f55804a0b995f62cbf3cb2
alex180500 <30732244+alex180500@users.noreply.github.com>
2026-05-11 18:50:12 +0200

    feat: added various function name to public

--- src/online_qml/__init__.py
+++ src/online_qml/__init__.py
@@ -3,9 +3,16 @@
 from .core import (
     LayerResult,
     MetricResult,
+    SimulationContext,
     SimulationData,
+    get_complex_dtype,
+    get_real_dtype,
     get_torch_device,
+    load_data,
+    load_simulation_data,
     logspace_int,
+    save_data,
+    save_simulation_data,
 )
 from .estimators import (
     LinearReadoutEstimator,
@@ -13,6 +20,19 @@ from .estimators import (
     ShadowReadoutEstimator,
 )
 from .evaluation import HaarBiasVariance
+from .experiments import (
+    evaluate_layer_result_haar,
+    make_layers_ntrain_grid,
+    make_layers_shot_grid,
+    measurement_frame_distance_grid,
+    state_frame_distance_grid,
+)
+from .quantum import (
+    sample_dm,
+    sample_povm,
+    sample_traceless_operator,
+    shots_outcome,
+)
 from importlib.metadata import version
 
 __all__ = [
@@ -21,10 +41,26 @@ __all__ = [
     "LinearReadoutEstimator",
     "MetricResult",
     "RunningOutcomeStats",
+    "SimulationContext",
     "ShadowReadoutEstimator",
     "SimulationData",
+    "evaluate_layer_result_haar",
+    "get_complex_dtype",
+    "get_real_dtype",
     "get_torch_device",
+    "load_data",
+    "load_simulation_data",
     "logspace_int",
+    "make_layers_ntrain_grid",
+    "make_layers_shot_grid",
+    "measurement_frame_distance_grid",
+    "sample_dm",
+    "sample_povm",
+    "sample_traceless_operator",
+    "save_data",
+    "save_simulation_data",
+    "shots_outcome",
+    "state_frame_distance_grid",
 ]
 
 __version__ = version("online-qml")
